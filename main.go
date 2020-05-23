1cb5cdde799cbe6913ac19d8f670f151ba2ebce3
michaelayao <juy075@ucsd.edu>
2020-05-23 08:48:37 +0000

    Add authorization

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ import (
 	"strconv"
 
 	"cloud.google.com/go/storage"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/olivere/elastic"
 	"github.com/pborman/uuid"
@@ -57,11 +59,20 @@ type Post struct {
 func main() {
 	fmt.Println("started-service")
 
-	r := nux.NewRouter()
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte(mySigningKey), nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
 
-	r.Handle("/post", http.HandlerFunc(handlerPost)).Methods("POST", "OPTIONS")
-	r.Handle("/search", http.HandlerFunc(handlerSearch)).Methods("GET", "OPTIONS")
-	r.Handle("/cluster", http.HandlerFunc(handlerCluster)).Methods("GET", "OPTIONS")
+	r := mux.NewRouter()
+
+	r.Handle("/post", jwtMiddleware.Handler(http.HandlerFunc(handlerPost))).Methods("POST", "OPTIONS")
+	r.Handle("/search", jwtMiddleware.Handler(http.HandlerFunc(handlerSearch))).Methods("GET", "OPTIONS")
+	r.Handle("/cluster", jwtMiddleware.Handler(http.HandlerFunc(handlerCluster))).Methods("GET", "OPTIONS")
+	r.Handle("/signup", http.HandlerFunc(handlerSignup)).Methods("POST", "OPTIONS")
+	r.Handle("/login", http.HandlerFunc(handlerLogin)).Methods("POST", "OPTIONS")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
@@ -77,12 +88,17 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Use username inside of token
+	user := r.Context().Value("user")
+	claims := user.(*jwt.Token).Claims
+	username := claims.(jwt.MapClaims)["username"]
+
 	// Get parameter from request
 	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
 	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
 
 	p := &Post{
-		User:    r.FormValue("user"),
+		User:    username.(string),
 		Message: r.FormValue("message"),
 		Location: Location{
 			Lat: lat,
@@ -280,7 +296,7 @@ func saveToGCS(r io.Reader, objectName string) (*storage.ObjectAttrs, error) {
 	return attrs, nil
 }
 
-func saveToES(post *Post, index string, id string) error {
+func saveToES(i interface{}, index string, id string) error {
 	client, err := elastic.NewClient(elastic.SetURL(ES_URL))
 	if err != nil {
 		return err
@@ -289,13 +305,12 @@ func saveToES(post *Post, index string, id string) error {
 	_, err = client.Index().
 		Index(index).
 		Id(id).
-		BodyJson(post).
+		BodyJson(i).
 		Do(context.Background())
 
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Post is saved to index: %s\n", post.Message)
 	return nil
 }
